67d60fd4af510ea85123c256bc0ca243cbad56ba
SimasDei <simondeiman@gmail.com>
2019-02-07 21:08:49 +0200

    implement the HOLD Score functionality

--- app.js
+++ app.js
@@ -8,16 +8,61 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-let scores, RoundScore, activePlayer, dice;
+let scores, RoundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
+/*
 document.querySelector(
   '#current-' + activePlayer
 ).innerHTML = `<em>${dice}</em>`;
+*/
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random Number
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display result
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`;
+
+  // 3. Updated round Score IF roll was NOT 1
+  if (dice !== 1) {
+    // Add Score
+    roundScore += dice;
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent =
+    scores[activePlayer];
+
+  // Check if Played Won
+});
