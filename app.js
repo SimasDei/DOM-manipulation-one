9bb0186f0f3937ca1e1cad70f0f6d1669e065ebf
SimasDei <simondeiman@gmail.com>
2019-02-07 21:21:01 +0200

    implement wining condition, add style class to the winning party

--- app.js
+++ app.js
@@ -41,19 +41,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
   } else {
     // Next Player
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 });
 
@@ -65,4 +53,30 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer];
 
   // Check if Played Won
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('#name-' + activePlayer).textContent =
+      'You are Winrar!';
+    document.querySelector('.dice').style.display = 'none';
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.add('winner');
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.remove('active');
+  } else {
+    // Next Player
+    nextPlayer();
+  }
 });
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+}
