7e99032db6510b303f7021c1cb7de7b4125d93b7
SimasDei <simondeiman@gmail.com>
2019-02-08 10:34:02 +0200

    add another dice to the game, set new winning conditions

--- challenges.js
+++ challenges.js
@@ -5,22 +5,19 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gameSession) {
     // 1. Random Number
-    let dice = Math.floor(Math.random() * 6) + 1;
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display result
-    let diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = `dice-${dice}.png`;
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = `dice-${dice1}.png`;
+    document.getElementById('dice-2').src = `dice-${dice2}.png`;
 
     // 3. Updated round Score IF roll was NOT 1
-    if (dice === 6 && prevDice === 6) {
-      // Total Score Lost
-      scores[activePlayer] = 0;
-      document.querySelector(`#score-${activePlayer}`).textContent = 0;
-      nextPlayer();
-    } else if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add Score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector(
         `#current-${activePlayer}`
       ).textContent = roundScore;
@@ -28,8 +25,6 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
       // Next Player
       nextPlayer();
     }
-
-    prevDice = dice;
   }
 });
 
@@ -41,11 +36,25 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent =
       scores[activePlayer];
 
+    let input = document.querySelector('.final-score').value;
+    console.log(input);
+    let winningScore;
+
+    // Undefined, 0, null or '' are COERCED to FALSE
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     // Check if Played Won
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent =
         'You are Winrar!';
-      document.querySelector('.dice').style.display = 'none';
+
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
+
       document
         .querySelector('.player-' + activePlayer + '-panel')
         .classList.add('winner');
@@ -69,7 +78,9 @@ function nextPlayer() {
 
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -80,7 +91,9 @@ function init() {
   roundScore = 0;
   gameSession = true;
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'block';
+  document.getElementById('dice-2').style.display = 'block';
+
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
